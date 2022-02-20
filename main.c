5739e59d3ec8a96fd54ee2b2054372fe22fb7d26
Navid Fayezi <navidfayezi.98@gmail.com>
2022-02-20 17:54:44 +0330

    Refactor main.c

--- main.c
+++ main.c
@@ -14,15 +14,15 @@ struct ip_address{
 
 struct packet_data
 {
-    int recurrance_number;
+    int appearance_number;
     char *udp_payload;
     uint16_t udp_payload_len;
 };
 
 // there was not any clear documention for "cb" function,
 // that is why I had to declare these global variables instead of passing them to "cb".
-uint16_t *current_j;
-uint16_t *input_j;
+uint16_t *j_counter;
+uint16_t *input_i;
 uint16_t *input_port_number;
 char *pattern;
 struct ip_address *input_ip;
@@ -82,7 +82,7 @@ struct ip_address* ip_string_to_struct(char ** ip_str){
     return ip;
 }
 
-int appearance_count_in_payload(char* payload, char* pattern, int payload_len, int pattern_len){
+int count_appearances(char* payload, char* pattern, int payload_len, int pattern_len){
     int i, j, flag, counter;
     counter = 0;
 
@@ -110,7 +110,7 @@ int match_rules(uint16_t in_port, uint16_t packet_port,
     if(packet_port == in_port && packet_ip->octets[0] == input_ip->octets[0] && packet_ip->octets[1] == input_ip->octets[1]
        && packet_ip->octets[2] == input_ip->octets[2] && packet_ip->octets[3] == input_ip->octets[3]){
 
-        return appearance_count_in_payload(udp_payload, in_pattern, payload_len, strlen(in_pattern));
+        return count_appearances(udp_payload, in_pattern, payload_len, strlen(in_pattern));
     }
     else{
         return 0;
@@ -119,10 +119,10 @@ int match_rules(uint16_t in_port, uint16_t packet_port,
 }
 
 void save_packet_data(int appearance_no, char *upd_payload, uint16_t upd_payload_len){
-    if(*current_j < *input_j){
+    if(*j_counter < *input_i){
         int i;
         struct packet_data *pckt = malloc(sizeof(struct packet_data));
-        pckt->recurrance_number = appearance_no;
+        pckt->appearance_number = appearance_no;
         pckt->udp_payload_len = upd_payload_len;
         pckt->udp_payload = malloc(sizeof(char) * upd_payload_len);
         // deep copy
@@ -130,8 +130,8 @@ void save_packet_data(int appearance_no, char *upd_payload, uint16_t upd_payload
             pckt->udp_payload[i] = upd_payload[i];
         }
         //matched_packets [(*current_j)] = malloc(sizeof(struct packet_data));
-        matched_packets [(*current_j)] = pckt;
-        (*current_j)++;
+        matched_packets [(*j_counter)] = pckt;
+        (*j_counter)++;
     }
 }
 
@@ -139,12 +139,17 @@ void write_to_file(){
     int i, j;
     FILE *output_file = fopen("out.txt", "w");
 
-    for(i = 0; i < *input_j; i++){
+    if(!output_file){
+        printf("Error while opening output file");
+        return;
+    }
+
+    for(i = 0; i < *input_i; i++){
         fprintf(output_file, "Payload: ");
         for(j = 0; j < matched_packets[i]->udp_payload_len; j++){
             fprintf(output_file, "%c", matched_packets[i]->udp_payload[j]);
         }
-        fprintf(output_file, "\nAppearances: %d\n", matched_packets[i]->recurrance_number);
+        fprintf(output_file, "\nAppearances: %d\n", matched_packets[i]->appearance_number);
 
     }
     fclose(output_file);
@@ -219,19 +224,19 @@ static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
 void get_input(char *arg1, char *arg2, char *arg3, char *arg4){
     char *input_ip_string;
     char *port_string = arg2;
-    char *j_string = arg3;
+    char *i_string = arg3;
     pattern = arg4;
     input_ip_string = arg1;
     input_port_number = malloc(sizeof(uint16_t));
-    current_j = malloc(sizeof(uint16_t));
+    j_counter = malloc(sizeof(uint16_t));
 
-    input_j = malloc(sizeof(uint16_t));  // maximum number of iteration, given by user
+    input_i = malloc(sizeof(uint16_t));  // maximum number of iteration, given by user
     //input_ip = malloc(sizeof(struct ip_address));
-    *input_j = str_to_uint(j_string);    // number of iterations
+    *input_i = str_to_uint(i_string);    // number of iterations
 
-    matched_packets = malloc(sizeof(struct packet_data*) * (*input_j));
+    matched_packets = malloc(sizeof(struct packet_data*) * (*input_i));
 
-    *current_j = 0;
+    *j_counter = 0;
     *input_port_number = str_to_uint(port_string);
     input_ip = ip_string_to_struct(ip_string_preprocess(input_ip_string));
     return;
@@ -240,14 +245,14 @@ void get_input(char *arg1, char *arg2, char *arg3, char *arg4){
 void free_heap(){
     int i;
 
-    for(i = 0; i < *input_j; i++){
+    for(i = 0; i < *input_i; i++){
         free(matched_packets[i]->udp_payload);
         free(matched_packets[i]);
     }
 
     free(input_port_number);
-    free(input_j);
-    free(current_j);
+    free(input_i);
+    free(j_counter);
     free(input_ip);
     free(matched_packets);
 
@@ -290,7 +295,7 @@ int main(int argc, char **argv)
     while ((rv = recv(fd, buf, sizeof(buf), 0)) && rv >= 0) {
 
         nfq_handle_packet(h, buf, rv);
-        if(*current_j >= *input_j)
+        if(*j_counter >= *input_i)
             break;
     }
 
